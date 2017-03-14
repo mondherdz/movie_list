18fbc8e6abacf14d70aca8d05d5aa57c822fcb04
mondherdz <mondher.1jam@gmail.com>
2017-03-14 15:27:20 +0100

    Add files via upload

---
+++ movie_list.js
@@ -0,0 +1,70 @@
+var movie1 = {
+	image : "http://movizland.com/wp-content/uploads/0underworld_blood_wars_ver6.png",
+	name : "Underworld: Blood Wars 2016",
+	brief : "يتمكن مصاصو الدماء من استعادة القوات الأصلية لخلق أنواع جديدة من الكائنات النصف مخلًّقة بواسطة (اليكساندر كورفينوس)، أما عن (مايكل)، فهو يستعيد ذكرياته باحثًا عن (سيلين) وابنته في محاولة لخلق من التعايش بين البشر والمخلوقات الجديدة في سلام من جديد.",
+	stars : "img/3.png",
+	youtube_trailler : "https://www.youtube.com/watch?v=r6FxROAHJH4"
+}
+var movie2 ={
+	image :"http://movizland.com/wp-content/uploads/Kong-Skull-Island-2017-490x640.jpg",
+	name:"Kong Skull Island 2017",
+	brief:"خلال حقبة السبعينات في الولايات المتحدة اﻷمريكية، يقوم مجموعة من المستكشفين والجنود بالخروج في رحلة شاقة نحو جزيرة نائية تقع في قلب المحيط الهادئ؛ وتبدأ التحريات حولها وحول ما تحتويه، وصولًا إلى كونج اﻷسطوري، ملك هذه الجزيرة وما عليها.",
+	stars:"img/3_5.png",
+	youtube_trailler:"https://www.youtube.com/watch?v=YAbI4w95cTE"
+}
+var movie3 ={
+	image :"http://movizland.com/wp-content/uploads/BkND-490x640.jpg",
+	name:"Zombies Have Fallen 2017",
+	brief:"وساعد امرأة شابة تمتلك قوى خارقة للطبيعة ورؤى نهاية العالم المقبلة التي كتبها صياد مكافأة المتقاعدين الذين يجب حمايتها من أولئك الذين يرغبون في استخدام قدراته للشر.",
+	stars:"img/3.png",
+	youtube_trailler:"https://www.youtube.com/watch?v=pa6HYKDeNmo"
+}
+var movie4 ={
+	image :"http://movizland.com/wp-content/uploads/0logan_ver5.png",
+	name:"Logan 2017",
+	brief:"تدور أحداث الفيلم في المستقبل، حيث ينبغي لـ(لوجان) و(البروفيسور تشارلز كزافييه) التعامل مع الخسائر التي سببها (ناثانيال إسيكس) بتدميره العالم وتركه في الخراب.. قدرات لوجان الشفائية الخارقة تشرع في التلاشي تدريجيًا، بينما يقود داء الزهايمر كزافييه إلى نسيان كل شيء؛ ويصبح على لوجان مواجهة ناثانيال إسيكس وإلحاق الهزيمة به بمعاونة فتاة شابة تًدعى (لورا كيني)، وهي الاستنساخ الأنثوي من وولفرين.",
+	stars:"img/4.png",
+	youtube_trailler:"https://www.youtube.com/watch?v=gbug3zTm3Ws"
+}
+var movie5 ={
+	image :"http://movizland.com/wp-content/uploads/0assassins_creed_ver2.png",
+	name:"Assassin’s Creed 2016",
+	brief:"من خلال ثورة التكنولوجيا العاتية والتي فتحت الطريق للذكريات الوراثية وشقّت سبيلًا لها، كالوم لينش (مايكل فاسبيندر) يختبر مغامرات جدّه (أجيلار) خلال القرن الخامس عشر في (إسبانيا)، ليكتشف كالوم خلال تجربته تلك أنه ينحدر من طبقات مجتمع سري غامض، لهم من المعرفة والقدرات ما لا يُصدق، وبمثابة قوة عاتية ولا تُردّع بوقتنا الحاضر.",
+	stars:"img/3.png",
+	youtube_trailler:"https://www.youtube.com/watch?v=C2e6Oruy_fA"
+}
+var movie6 ={
+	image :"http://movizland.com/wp-content/uploads/me-toys-2016-490x640.jpg",
+	name:"Time Toys 2016",
+	brief:"حول مجموعة من أطفال المدرسة يقع فى طريقهم صندوق ممتليء بألعاب من المستقبل. هذا الاكتشاف يأخذهم في مغامرة مثيرة باستخدام ألعابهم الجديدة لانقاذ حيهم والعالم من رجل اعمال مجنون. في هذه العملية هؤلاء الأطفال الفاشلين يتعلمون أن لديهم الصفات التي ظنوا أنها ليست عندهم.",
+	stars:"img/2_5.png",
+	youtube_trailler:"https://www.youtube.com/watch?v=EbXn-vLXy80"
+}
+var movie7 ={
+	image :"http://movizland.com/wp-content/uploads/0miss_sloane_ver2.png",
+	name:"Miss Sloane 2016",
+	brief:"إليزابيث سلون (جيسيكا تشاستين)، سياسية قاسية وناجحة للغاية في عملها، واعتادت دومًا على الانتصار في معاركها السياسية بكل الوسائل الممكنة في العاصمة اﻷمريكية واشنطن، وعندما تصير في مواجهة أقوى خصم سياسي على اﻹطلاق في مسيرتها العملية، تدرك أن النجاح سياتي بثمن باهظ.",
+	stars:"img/3_5.png",
+	youtube_trailler:"https://www.youtube.com/watch?v=AMUkfmUu44k"
+}
+var movie8 ={
+	image :"http://movizland.com/wp-content/uploads/600x600bb-179.jpg",
+	name:"The Last Scout 2017",
+	brief:"حول بعد أن دُمرت الأرض في الحرب يقوم فريق من رواد الفضاء الكشافة يبحثون فى الكون عن مكان يمكن العيش فيه من اجل إنقاذ الجنس البشري.",
+	stars:"img/2_5.png",
+	youtube_trailler:"https://www.youtube.com/watch?v=fJBiOYioqJM"
+}
+var movie9 ={
+	image :"http://movizland.com/wp-content/uploads/0Burning-Sands-2017.png",
+	name:"Burning Sands 2017",
+	brief:"",
+	stars:"img/3.png",
+	youtube_trailler:"https://www.youtube.com/watch?v=8kyOvsbj_hs"
+}
+var movie10 ={
+	image :"http://movizland.com/wp-content/uploads/hy_him-490x640.jpg",
+	name:"Why Him 2016",
+	brief:"خلال فترة الأعياد، نيد 'برايان كرانستون' الأب المُحافظ حد الإفراط - لكن مع حبه البالغ لعائلته - يتوجه بأسرته لزيارة ابنته في جامعة 'ستانفورد'، حيث يقابل أعتى كوابيسه هناك، وهو صديقها حسن النية الأحمق اجتماعيًا، البليونير لايرد 'جيمس فرانكو'. والذي يعتقده نيد شخص غير لائق تمامًا لابنته..",
+	stars:"img/3_5.png",
+	youtube_trailler:"https://www.youtube.com/watch?v=qksQSt8K830"
+}
